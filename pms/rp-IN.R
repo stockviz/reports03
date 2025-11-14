61fbc65f3060e9c736624500a50806e95bce0707
Shyam Sunder <shyam.sunder@stockviz.biz>
2025-11-14 10:13:13 +0530

    india daily

--- pms/rp-IN.R
+++ pms/rp-IN.R
@@ -17,25 +17,59 @@ source("/mnt/hollandr/config.r")
 lcon <- odbcDriverConnect(sprintf("Driver={ODBC Driver 17 for SQL Server};Server=%s;Database=%s;Uid=%s;Pwd=%s;", ldbserver, ldbname, ldbuser, ldbpassword), case = "nochange", believeNRows = TRUE)
 pgCon <- dbConnect(RPostgres::Postgres(), host = 'sweden', user = ldbuser2, password = ldbpassword2, dbname = 'StockVizDyn', sslmode = 'allow')
 
-mktBenches <- c("NIFTY 50 TR", "NIFTY MIDCAP 150 TR", "NIFTY SMALLCAP 250 TR", "NIFTY MICROCAP 250 TR")
-monthlyBenchRets <- NULL
 startDate <- as.Date("2021-01-01")
-for(i in 1:length(mktBenches)){
-  pDf <- sqlQuery(lcon, sprintf("select px_close, time_stamp from bhav_index where index_name = '%s' and time_stamp >= '%s'", mktBenches[i], startDate))
-  monthlyBenchRets <- merge.xts(monthlyBenchRets, monthlyReturn(xts(pDf[,1], pDf[,2])))
+getMonthlyIndexRets <- function(iNames){
+  rets <- NULL
+  for(i in 1:length(iNames)){
+    pDf <- sqlQuery(lcon, sprintf("select px_close, time_stamp from bhav_index where index_name = '%s' and time_stamp >= '%s'", iNames[i], startDate))
+    rets <- merge.xts(rets, monthlyReturn(xts(pDf[,1], pDf[,2])))
+  }
+  
+  index(rets) <- as.Date(sprintf("%d-%d-%d", year(index(rets)), month(index(rets)), 
+                                                days_in_month(as.Date(sprintf("%d-%d-1", year(index(rets)), month(index(rets)))))))
+  
+  names(rets) <- iNames
+  return(rets)
 }
 
-index(monthlyBenchRets) <- as.Date(sprintf("%d-%d-%d", year(index(monthlyBenchRets)), month(index(monthlyBenchRets)), 
-                                           days_in_month(as.Date(sprintf("%d-%d-1", year(index(monthlyBenchRets)), month(index(monthlyBenchRets)))))))
+getMonthlyAvgMf <- function(schemeCodes){
+  rets <- NULL
+  for(i in 1:length(schemeCodes)){
+    pDf <- sqlQuery(lcon, sprintf("select nav, as_of from mf_nav_history where scheme_code = %d and as_of >= '%s'", schemeCodes[i], startDate))
+    rets <- merge.xts(rets, monthlyReturn(xts(pDf[,1], pDf[,2])))
+  }
+  
+  index(rets) <- as.Date(sprintf("%d-%d-%d", year(index(rets)), month(index(rets)), 
+                                 days_in_month(as.Date(sprintf("%d-%d-1", year(index(rets)), month(index(rets)))))))
+  
+  rets <- xts(rowMeans(rets, na.rm = TRUE), index(rets))
+  return(rets)
+}
 
-names(monthlyBenchRets) <- mktBenches
+mktBenches <- c("NIFTY 50 TR", "NIFTY MIDCAP 150 TR", "NIFTY SMALLCAP 250 TR", "NIFTY MICROCAP 250 TR")
+factorBenches <- c("NIFTY500 VALUE 50 TR", "NIFTY500 QUALITY 50 TR", "NIFTY500 MOMENTUM 50 TR")
+hybridFunds <- c(119609, #SBI EQUITY HYBRID FUND - DIRECT PLAN - Growth
+                 120251, #ICICI Prudential Equity & Debt Fund - Direct Plan - Growth
+                 119062 #HDFC Hybrid Equity Fund - Growth Plan
+                 )
+debtFunds <- c(118987, #HDFC Corporate Bond Fund - Growth Option - Direct Plan 
+               120692, #ICICI Prudential Corporate Bond Fund - Direct Plan - Growth
+               119533 #Aditya Birla Sun Life Corporate Bond Fund - Growth - Direct Plan
+              )
+               
+monthlyMktBenchRets <- getMonthlyIndexRets(mktBenches)
+monthlyFactorBenchRets <- getMonthlyIndexRets(factorBenches)
+monthlyHybridBenchRets <- getMonthlyAvgMf(hybridFunds)
+names(monthlyHybridBenchRets) <- c('HYBRID_MF')
+monthlyCorpDebtBenchRets <- getMonthlyAvgMf(debtFunds)
+names(monthlyCorpDebtBenchRets) <- c('CORP_DEBT_MF')
 
 today <- Sys.Date()
 
 idCnts <- dbGetQuery(pgCon, "select id, count(val) cnt, max(yr) yr from sebi_pms_data group by id")
 activePms <- idCnts |> filter(cnt >= 12 & yr >= year(today) -1)
 
-fileTracker <- data.frame(ID = 0, STRAT_FILE = "", STRAT_TITLE = "")
+fileTracker <- data.frame(ID = 0, STRAT_FILE = "", STRAT_TITLE = "", ASSET_CLASS = "")
 
 for(i in 1:nrow(activePms)){
   metaId <- activePms$id[i]
@@ -97,45 +131,58 @@ for(i in 1:nrow(activePms)){
     select(dt, name, ret) |> distinct(dt, name, .keep_all = TRUE) |>
     pivot_wider(id_cols = dt, names_from = name, values_from = ret)
   
-  names2Remove <- (strategyRets |> summarise(across(everything(), ~sum(is.na(.)))) |>
-    pivot_longer(cols = everything()) |>
-    filter(value > nrow(strategyRets)*0.9) |>
-    select(name))$name
+  lastIsNa <- strategyRets |> filter(dt == max(dt)) |> summarise(across(everything(), ~sum(is.na(.)))) |> pivot_longer(cols = everything()) |> rename(v1 = value)
+  naCount <- strategyRets |> summarise(across(everything(), ~sum(is.na(.)))) |> pivot_longer(cols = everything()) |> rename(v2 = value)
   
+  names2Remove <- (lastIsNa |> inner_join(naCount, by = join_by(name)) |>
+    filter(v2 > nrow(strategyRets)*0.9 | (v1 != 0 & v2 > nrow(strategyRets)*0.5)) |>
+    select(name))$name
+                                                                                             
   strategyRets <- strategyRets |> select(-all_of(names2Remove))
   
   for(j in 2:ncol(strategyRets)){
+    if(nrow(na.omit(strategyRets[,j])) < 12) next
+    
+    stratTname <- colnames(strategyRets)[j]
+    stratFname <- gsub(' ', '_', stratTname)
+    
+    sebiClass <- ""
+    tryCatch({
+      sebiClass <- (stratRetTb |> filter(name == stratTname & !is.na(strategy) & !is.null(strategy)) |> select(strategy))[1, 1][[1]]
+    }, error = \(x) {})
+    
     srXts <- xts(strategyRets[,j], strategyRets$dt)
     srXts <- tail(srXts, 24)
     
     names(srXts)[1] <- "Strategy"
-    retXts <- merge(srXts, monthlyBenchRets)
-    retXts <- na.omit(retXts)
     
-    stratFname <- gsub(' ', '_', colnames(strategyRets)[j])
-    stratTname <- str_to_title(colnames(strategyRets)[j])
+    if(sebiClass == "EQUITY"){
+      retXts <- merge(srXts, monthlyMktBenchRets)
+      retXts <- na.omit(retXts)
+    } else if (sebiClass == "HYBRID"){
+      retXts <- merge(srXts, monthlyHybridBenchRets)
+      retXts <- na.omit(retXts)
+    } else if (sebiClass == "DEBT"){
+      retXts <- merge(srXts, monthlyCorpDebtBenchRets)
+      retXts <- na.omit(retXts)
+    } else {
+      retXts <- srXts
+    }
+    
     sr <- paste(round(SharpeRatio.annualized(retXts), 2), collapse="/")
     Common.PlotCumReturns(retXts, sprintf("%s Returns", stratTname), sprintf("SR: %s", sr), #NULL)
                           sprintf("%s/pms-%d.%s.cum.png", reportPath, metaId, stratFname))
     
-    toPlot <- data.frame(retXts*100)
-    toPlot$T <- index(retXts)
+    if(sebiClass == "EQUITY"){
+      retXts <- merge(srXts, monthlyFactorBenchRets)
+      retXts <- na.omit(retXts)
     
-    ggplot(toPlot |> pivot_longer(cols=-T), aes(x=T, y=value, fill = name)) +
-      theme_economist() +
-      theme(axis.text.x = element_text(angle = 90, hjust = 1)) +
-      geom_bar(stat = "identity", position = position_dodge()) +
-      scale_x_date(date_breaks = "3 months", date_labels = "%Y-%b") +
-      scale_fill_viridis_d() +
-      labs(x='', y='returns (%)', fill = '',
-           title = sprintf("%s Returns", stratTname),
-           subtitle = sprintf("%s:%s", min(toPlot$T), max(toPlot$T)),
-           caption = '@StockViz')
+      sr <- paste(round(SharpeRatio.annualized(retXts), 2), collapse="/")
+      Common.PlotCumReturns(retXts, sprintf("%s Returns", stratTname), sprintf("SR: %s", sr), #NULL)
+                          sprintf("%s/pms-%d.%s.factor.cum.png", reportPath, metaId, stratFname))
+    }
     
-    ggsave(sprintf("%s/pms-%d.%s.monthly.png", reportPath, metaId, stratFname), 
-           width=12, height=6, units="in")
-    
-    fileTracker <- rbind(fileTracker, c(metaId, stratFname, stratTname))
+    fileTracker <- rbind(fileTracker, c(metaId, stratFname, stratTname, sebiClass))
   }
 }
 
